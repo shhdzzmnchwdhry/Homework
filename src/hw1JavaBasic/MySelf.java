8c3240e822bf8fdaf70f400a8d322ee1596f638a
shhdzzmnchwdhry <shhdzzmnchwdhry@gmail.com>
2023-01-24 23:12:37 -0500

    hw1

---
+++ src/hw1JavaBasic/MySelf.java
@@ -0,0 +1,15 @@
+package hw1JavaBasic;
+
+/* Project name starts with upper case
+ * package name starts with lower case 
+ * Class name starts with upper case
+ */
+public class MySelf {
+	// main method is the entry point of Java
+	public static void main(String[] args) {
+		System.out.println("Shahiduzzaman Chowdhury");
+
+		System.out.println("32-61 92 st. , East Elmhurst, NY11369.");
+	}
+
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,9 @@
+package hw2JavaBasics;
+
+public class MyCar {
+	public static void main(String[] args) {
+		System.out.println("My name is :"+"\"Shahiduzzaman\""+"I love to drive,"+"My car info is below:");
+	
+}
+
+}
